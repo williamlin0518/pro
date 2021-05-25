2f255177afd49a4521b941407a8c59dd4c5a6993
Hunk0724 <84437755+Hunk0724@users.noreply.github.com>
2021-05-25 13:11:29 +0800

    Update SettingSubScene.java

--- src/SubScene/SettingSubScene.java
+++ src/SubScene/SettingSubScene.java
@@ -13,7 +13,10 @@ import javafx.util.Duration;
 public class SettingSubScene extends SubScene {
 
     private boolean isHidden = true;
-
+    Button backtogame = new Button("Back to game");//Hunk0525
+	Button keysetting = new Button("Key setting");//Hunk0525
+	Button backtomenu = new Button("Back to menu");//Hunk0525
+	
 
     private final static String BACKGROUND_IMAGE = "file:/Users/linchengwei/IdeaProjects/SpaceRunner2/src/SubScene/openletter.png";
 
@@ -25,13 +28,78 @@ public class SettingSubScene extends SubScene {
         BackgroundImage image = new BackgroundImage(new Image(BACKGROUND_IMAGE, 400, 500, false, true),
                 BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, null);
 
-        AnchorPane root2 = (AnchorPane) this.getRoot();
+        AnchorPane rootset = (AnchorPane) this.getRoot();
 
-        root2.setBackground(new Background(image));
+        rootset.setBackground(new Background(image));
 
-        setLayoutX(250);
+        setLayoutX(250);//Hunk0525 35-50
         setLayoutY(-550);
+         setLayoutX(-100);
+        setLayoutY(-1000);
+        backtogame.setPrefSize(100, 50);
+        backtogame.setLayoutX(300);
+        backtogame.setLayoutY(300);
+        keysetting.setPrefSize(100, 50);
+        keysetting.setLayoutX(450);
+        keysetting.setLayoutY(300);
+        backtomenu.setPrefSize(100, 50);
+        backtomenu.setLayoutX(600);
+        backtomenu.setLayoutY(300);
+        rootset.getChildren().add(backtogame);
+        rootset.getChildren().add(keysetting);
+        rootset.getChildren().add(backtomenu);//Hunk0525
+        
+        backtogame.setOnAction(new EventHandler<ActionEvent>() {
+        	
+			@Override
+			public void handle(ActionEvent actionEvent) {
+				// TODO Auto-generated method stub
+				moveSubScene();
+			}
+        	
+        });//Hunk0525
+        
+      keysetting.setOnAction(new EventHandler<ActionEvent>() {
 
+			@Override
+			public void handle(ActionEvent actionEvent) {
+				// TODO Auto-generated method stub
+				 ImageView keyBoard=  new ImageView(new Image(getClass().getResourceAsStream("keyBoard.jpg")));
+				 rootLetter.getChildren().add(keyBoard);
+				 keyBoard.setX(100);
+				 keyBoard.setY(200);
+				 
+				 Button backtosetting = new Button("Back to setting");
+					backtosetting.setPrefSize(200, 50);
+			        backtosetting.setLayoutX(450);
+			        backtosetting.setLayoutY(700);
+			        rootLetter.getChildren().add(backtosetting);
+			        backtosetting.setOnAction(new EventHandler<ActionEvent>() {
+			        	
+						@Override
+						public void handle(ActionEvent arg0) {
+							
+							// TODO Auto-generated method stub
+							keyBoard.setVisible(false);
+							backtosetting.setVisible(false);
+						}
+			        	
+			        });
+			}
+			
+        });//Hunk0525
+        
+        backtomenu.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent actionEvent) {
+				// TODO Auto-generated method stub
+				
+			}
+        	
+        });
+
+    }//Hunk0525
     }
 
     public void moveSubScene() {
