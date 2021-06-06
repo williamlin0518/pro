715ba071e566d22c7ce14e7b73bf07d6bef658a3
hyi1014 <80416477+hyi1014@users.noreply.github.com>
2021-06-06 20:32:20 +0800

    Add files via upload

---
+++ Lab2/ShopView.java
@@ -0,0 +1,250 @@
+package Lab2;
+import Player.Player;
+import Setting.Setting;
+import SubScene.SettingSubScene;
+import javafx.animation.AnimationTimer;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import view.GameView;
+
+import static java.lang.Math.abs;
+
+public class ShopView {
+    private Scene gameScene;
+    private Stage shopStage;
+    private Stage firstviewStage;
+    boolean createFirstView=true;
+    boolean isHidden1,isHidden2,isHidden3,isHiddendialogBox;
+    boolean shopisactive;
+    private final Pane shopRoot =new Pane();
+    ImageView imageView = new ImageView(new Image(getClass().getResourceAsStream(("playerImageNew.png"))));
+    ImageView top = new ImageView(new Image(getClass().getResourceAsStream("top.png"), 600, 400, false, false));
+    ImageView middle = new ImageView(new Image(getClass().getResourceAsStream("middle.png"), 600, 400, false, false));
+    ImageView bottom = new ImageView(new Image(getClass().getResourceAsStream("bottom.png"), 600, 400, false, false));
+    ImageView dialogBox = new ImageView(new Image(getClass().getResourceAsStream("test2.png"), 600, 200, false, false));
+    ImageView lock = new ImageView(new Image(getClass().getResourceAsStream("lock.png"), 300, 500, false, false));
+    ImageView merchantView = new ImageView(new Image(getClass().getResourceAsStream(("merchant2.png"))));
+//    ImageView labelEmpty =new ImageView(new Image(getClass().getResourceAsStream("labelEmpty.png")));
+    Player playerShop =new Player(imageView);
+    private double playerX;
+    private double playerY;
+    SettingSubScene settingSubScene;
+    private Label keyLabelPane =new Label();
+    public ShopView(boolean isBridge) {
+        top.setFitWidth(600);
+        top.setFitHeight(400);
+        top.setX(50);
+        top.setY(100);
+
+        middle.setFitWidth(600);
+        middle.setFitHeight(400);
+        middle.setX(50);
+        middle.setY(100);
+
+        bottom.setFitWidth(600);
+        bottom.setFitHeight(500);
+        bottom.setX(50);
+        bottom.setY(100);
+
+
+        lock.setFitWidth(300);
+        lock.setFitHeight(500);
+        lock.setX(300);
+        lock.setY(50);
+
+        dialogBox.setFitWidth(600);
+        dialogBox.setFitHeight(200);
+        dialogBox.setX(60);
+        dialogBox.setY(450);
+
+        isHidden1  =true;
+        isHidden2 = true;
+        isHidden3 = true;
+        isHiddendialogBox=true;
+        gameScene = new Scene(shopRoot, GameView.GAME_WIDTH, GameView.GAME_HEIGHT);
+//        gameScene.getStylesheets().add(getClass().getResource("view\\view.css").toExternalForm());
+        shopStage = new Stage();
+        shopStage.setScene(gameScene);
+        //settingButton.setOnAction(new EventHandler<ActionEvent>() {
+
+        //	@Override
+        //  public void handle(ActionEvent actionEvent) {
+        //      GameView gameView=new GameView(870,680,false);
+        //      gameView.createNewGame(room1Stage);
+        ///   }
+        // });
+        gameScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.UP)   { GameView.direction= GameView.Direction.up;GameView.up=true;}
+                if (event.getCode() == KeyCode.DOWN) { GameView.direction= GameView.Direction.down; GameView.down=true;}
+                if (event.getCode() == KeyCode.RIGHT){ GameView.direction= GameView.Direction.right; GameView.right=true;}
+                if (event.getCode() == KeyCode.LEFT) { GameView.direction= GameView.Direction.left; GameView.left=true;}
+                if (event.getCode() == KeyCode.S)    { shopisactive=true;}
+            }});
+        gameScene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.UP)    { GameView.up=false; }
+                if (event.getCode() == KeyCode.DOWN)  { GameView.down=false; }
+                if (event.getCode() == KeyCode.RIGHT) { GameView.right=false; }
+                if (event.getCode() == KeyCode.LEFT)  { GameView.left=false; }
+                if (event.getCode() == KeyCode.S)     { shopisactive=false;
+                System.out.println("shopView check");
+                System.out.println(playerX + ", " + playerY);}
+            }});
+        int merchantX=550,merchantY=150;
+        shopRoot.getChildren().add(merchantView);merchantView.setX(merchantX);merchantView.setY(merchantY);
+        shopRoot.getChildren().add(playerShop);
+        shopRoot.getChildren().add(keyLabelPane);
+        playerShop.setTranslateX(0);
+        playerShop.setTranslateY(0);
+        AnimationTimer timer=new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                playerX= playerShop.getTranslateX();
+                playerY= playerShop.getTranslateY();
+                /*
+                if(playerX==0&&playerY==GameView.GAME_HEIGHT-Player.PLAYER_HEIGHT&&GameView.direction.equals(GameView.Direction.down)&& goLivingRoom){
+                    double playerX=16*Player.PACE_SIZE,playerY=16*Player.PACE_SIZE;
+                    GameView gameView=new GameView(playerX,playerY,enterFatherRoom);
+                    System.out.println("go LivingRoom");
+                    gameView.createNewGame(room1Stage);
+                    goLivingRoom =false;
+
+                }
+                if ( r1isactive&&(abs(playerX+Player.PLAYER_WIDTH-GameView.GAME_WIDTH) == 0) && (abs(playerY) == 0)) {
+                    System.out.println("open box");
+                    textArea.appendText("\nopen box");
+                    keyLabelPane.setGraphic(keyLabel);
+                    textArea.appendText("\nget fatherRoom key");
+                    enterFatherRoom =true;
+                    r1isactive = false;
+                }
+                if( r1isactive){
+                    System.out.println("check");
+                    textArea.appendText("\ncheck");
+                    System.out.println(playerX+" , "+playerY);
+                    r1isactive=false;
+                }
+
+                 */
+
+                if(shopisactive&&abs(playerX-merchantX)<50&&abs(playerY-merchantY)<50){
+                    if(isHiddendialogBox) {
+                        shopRoot.getChildren().add(dialogBox);
+                        isHiddendialogBox=false;
+                        System.out.println("open");
+                    }else{
+                        shopRoot.getChildren().remove(dialogBox);
+                        isHiddendialogBox=true;
+                        System.out.println("close");
+                    }
+                    shopisactive=false;
+                }
+                /*
+                if(shopisactive&&abs(playerX-merchantX)<50&&abs(playerY-merchantY)<50){
+                    shopSubScene.setVisible(true);
+                    shopisactive=false;
+                    System.out.println("open");
+                }
+
+                 */
+                if(shopisactive&&playerY>360&&playerY<400&&playerX>60&&playerX<140) {
+                    if(isHidden2) {
+                        shopRoot.getChildren().add(middle);
+                        isHidden2=false;
+                        //System.out.println("middle");
+                    }else{
+                        shopRoot.getChildren().remove(middle);
+                        isHidden2=true;
+                        //System.out.println("middle middle");
+                    }
+                    shopisactive=false;
+
+                }
+                if(shopisactive&&playerX>40&&playerX<260&&playerY>490&&playerY<560) {
+
+                    if(isHidden3) {
+                        shopRoot.getChildren().add(bottom);
+                        isHidden3=false;
+                        //System.out.println("top");
+                    }else{
+                        shopRoot.getChildren().remove(bottom);
+                        isHidden3=true;
+                        //System.out.println("top top");
+                    }
+                    shopisactive=false;
+                }
+
+                if(shopisactive&&playerX>470&&playerX<550&&playerY>180&&playerY<250) {
+
+                    if(isHidden3) {
+                        shopRoot.getChildren().add(top);
+                        isHidden3=false;
+                        System.out.println("hi");
+                    }else{
+                        shopRoot.getChildren().remove(top);
+                        isHidden3=true;
+                        System.out.println("hi hi");
+                    }
+                    shopisactive=false;
+
+                }
+                if(shopisactive&&playerX>532&&playerX<732&&playerY>448&&playerY<648) {
+
+                    if(isHidden3) {
+                        shopRoot.getChildren().add(lock);
+                        isHidden3=false;
+                        System.out.println("hi");
+                    }else{
+                        shopRoot.getChildren().remove(lock);
+                        isHidden3=true;
+                        System.out.println("hi hi");
+                    }
+                    shopisactive=false;
+                }
+
+
+                playerShop.updateShop(GameView.up,GameView.down,GameView.right,GameView.left);
+            }
+        };
+        timer.start();
+        createGameBackground();
+        createSettingSubScene();//
+    }
+    public void createShopView(Stage firstviewStage){
+        // pane.getChildren().remove(player);
+        playerShop.setTranslateX(playerX);
+        playerShop.setTranslateY(playerY);
+
+        this.firstviewStage=firstviewStage;
+        this.firstviewStage.close();
+        shopStage.show();
+    }
+
+    private void createGameBackground(){
+        Image backgroundImage= new Image("Lab2\\shop.jpg",GameView.GAME_WIDTH,GameView.GAME_HEIGHT,false,true);
+        BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.REPEAT,BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT,null);
+        shopRoot.setBackground(new Background(background));
+    }
+
+    private void createSettingSubScene(){
+        settingSubScene=new SettingSubScene();
+        shopRoot.getChildren().add(settingSubScene);
+    }
+
+
+
+}
---
+++ Player/Player.java
@@ -0,0 +1,297 @@
+package Player;
+
+import javafx.geometry.Rectangle2D;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+import view.GameView;
+
+public class Player extends Pane {
+    public static final int PACE_SIZE = 3;
+	public static int UNIT_SIZE=3;
+    ImageView playerSize;
+    public static int PLAYER_WIDTH=32*UNIT_SIZE;
+    public static int PLAYER_HEIGHT=40*UNIT_SIZE;
+    int PLAYER_X = 0;int PLAYER_Y = 0;
+    int DELAY=200;
+    SpriteAnimation animation;
+    public Player(ImageView imageView){
+        this.playerSize =imageView;
+        this.playerSize.setViewport(new Rectangle2D(PLAYER_X, PLAYER_Y, PLAYER_WIDTH, PLAYER_HEIGHT));
+        animation =new SpriteAnimation(imageView, Duration.millis(DELAY), SpriteAnimation.rowImage, SpriteAnimation.columnImage, PLAYER_X, PLAYER_Y, PLAYER_WIDTH, PLAYER_HEIGHT);
+        getChildren().addAll(imageView);
+    }
+    public void moveX(int moveX){
+        for(int i=0;i<Math.abs(moveX);i++){
+            if(GameView.right){ this.setTranslateX(this.getTranslateX()+1); }
+            if(GameView.left) { this.setTranslateX(this.getTranslateX()-1); }
+        }
+    }
+    public void moveY(int moveY){
+        for(int j=0;j<Math.abs(moveY);j++){
+            if(GameView.down)   { this.setTranslateY(this.getTranslateY()+1); }
+            if(GameView.up) { this.setTranslateY(this.getTranslateY()-1); }
+        }
+    }
+    public void updateGameView(boolean up, boolean down, boolean right, boolean left){
+        if(up){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*1);
+            if(unMovableUpGameView()){moveY(0);}
+            else{this.moveY(UNIT_SIZE+3);}
+        }
+        else if(down){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*0);
+            if(unMovableDownGameView()){moveY(0);}
+            else{this.moveY(UNIT_SIZE+3);}
+        }
+        else if(right){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*3);
+            if(unMovableRightGameView()){moveX(0);}
+            else {this.moveX(UNIT_SIZE+3);}
+        }
+        else if(left){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*2);
+            if(unMovableLeftGameView()){moveX(0);}
+            else{this.moveX(UNIT_SIZE+3);}
+        }
+        else this.animation.stop();
+    }
+    public void updateFirstView(boolean up, boolean down, boolean right, boolean left){
+        if(up){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*1);
+            
+            this.moveY(UNIT_SIZE+3);
+        }
+        else if(down){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*0);
+           
+            this.moveY(UNIT_SIZE+3);
+        }
+        else if(right){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*3);
+            
+            this.moveX(UNIT_SIZE+3);
+        }
+        else if(left){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*2);
+           
+            this.moveX(UNIT_SIZE+3);
+        }
+        else this.animation.stop();
+    }
+    public void updateShop(boolean up, boolean down, boolean right, boolean left){
+        if(up){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*1);
+
+            this.moveY(UNIT_SIZE+3);
+        }
+        else if(down){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*0);
+
+            this.moveY(UNIT_SIZE+3);
+        }
+        else if(right){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*3);
+
+            this.moveX(UNIT_SIZE+3);
+        }
+        else if(left){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*2);
+
+            this.moveX(UNIT_SIZE+3);
+        }
+        else this.animation.stop();
+    }
+    int leftAisleWidth =74*Player.UNIT_SIZE, upAisleHeight =2* UNIT_SIZE, downAisleHeight =85* UNIT_SIZE,rightAisleWidth=0* UNIT_SIZE;
+    int cabinetWidth =67* UNIT_SIZE, cabinetHeight =52* UNIT_SIZE;
+    public boolean unMovableUpGameView() {
+        if (!GameView.direction.equals(GameView.Direction.down)) {
+            //���
+            if (this.getTranslateY() == 0) { return true; }
+            //�U�����D
+            else if (this.getTranslateX()> leftAisleWidth&&this.getTranslateX()<GameView.GAME_WIDTH&&this.getTranslateY()==GameView.GAME_HEIGHT- downAisleHeight){return true;}
+            //
+            else {return false;}
+        }
+        else {return false;}
+    }
+    public boolean unMovableDownGameView() {
+        if (!GameView.direction.equals(GameView.Direction.up)) {
+            //���
+            if (this.getTranslateY()+PLAYER_HEIGHT==GameView.GAME_HEIGHT) { return true; }
+            //���U�d�l
+            else if (this.getTranslateX() <cabinetWidth&& this.getTranslateY()+PLAYER_HEIGHT==GameView.GAME_HEIGHT-cabinetHeight){return true;}
+            //�W�����D
+            else if (this.getTranslateX()>leftAisleWidth&&this.getTranslateX()<GameView.GAME_WIDTH&&this.getTranslateY()== upAisleHeight){return true;}
+            //
+            else {return false;}
+        }
+        else {return false;}
+    }
+    public boolean unMovableLeftGameView() {
+        if (!GameView.direction.equals(GameView.Direction.right)) {
+            //���
+            if (this.getTranslateX() == 0) { return true; }
+            //���U�d�l
+            else if (this.getTranslateX() == cabinetWidth && this.getTranslateY() + PLAYER_HEIGHT > GameView.GAME_HEIGHT - cabinetHeight){return true;}
+            //
+            else {return false;}
+        }
+        else {return false;}
+    }
+    public boolean unMovableRightGameView() {
+        if (!GameView.direction.equals(GameView.Direction.left)) {
+            //���
+            if (this.getTranslateX()+PLAYER_WIDTH ==GameView.GAME_WIDTH) { return true; }
+            //���䨫�
+            else if (this.getTranslateX()== leftAisleWidth&& this.getTranslateY() > upAisleHeight&&this.getTranslateY()<GameView.GAME_HEIGHT- downAisleHeight){return true;}
+            //
+            else {return false;}
+        }
+        else {return false;}
+    }
+    public void updateRoom1(boolean up, boolean down, boolean right, boolean left){
+        if(up){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*1);
+            if(unMovableUpRoom1()){moveY(0);}
+            else{this.moveY(UNIT_SIZE);}
+        }
+        else if(down){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*0);
+            if(unMovableDownRoom1()){moveY(0);}
+            else{this.moveY(UNIT_SIZE);}
+        }
+        else if(right){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*3);
+            if(unMovableRightRoom1()){moveX(0);}
+            else {this.moveX(UNIT_SIZE);}
+        }
+        else if(left){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*2);
+            if(unMovableLeftRoom1()){moveX(0);}
+            else{this.moveX(UNIT_SIZE);}
+        }
+        else this.animation.stop();
+    }
+    public boolean unMovableUpRoom1() {
+        if (!GameView.direction.equals(GameView.Direction.down)) {
+            //���
+            if (this.getTranslateY() == 0) { return true; }
+            //
+            else {return false;}
+        }
+        else {return false;}
+    }
+    public boolean unMovableDownRoom1() {
+        if (!GameView.direction.equals(GameView.Direction.up)) {
+            //���
+            if (this.getTranslateY()+PLAYER_HEIGHT==GameView.GAME_HEIGHT) { return true; }
+            //
+            else {return false;}
+        }
+        else {return false;}
+    }
+    public boolean unMovableLeftRoom1() {
+        if (!GameView.direction.equals(GameView.Direction.right)) {
+            //���
+            if (this.getTranslateX() == 0) { return true; }
+            //���U�d�l
+            //
+            else {return false;}
+        }
+        else {return false;}
+    }
+    public boolean unMovableRightRoom1() {
+        if (!GameView.direction.equals(GameView.Direction.left)) {
+            //���
+            if (this.getTranslateX()+PLAYER_WIDTH ==GameView.GAME_WIDTH) { return true; }
+            //
+            else {return false;}
+        }
+        else {return false;}
+    }
+    public void updateFatherRoom(boolean up, boolean down, boolean right, boolean left){
+        if(up){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*1);
+            if(unMovableUpFatherRoom()){moveY(0);}
+            else{this.moveY(UNIT_SIZE);}
+        }
+        else if(down){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*0);
+            if(unMovableDownFatherRoom()){moveY(0);}
+            else{this.moveY(UNIT_SIZE);}
+        }
+        else if(right){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*3);
+            if(unMovableRightFatherRoom()){moveX(0);}
+            else {this.moveX(UNIT_SIZE);}
+        }
+        else if(left){
+            this.animation.play();
+            this.animation.setOffsetY(PLAYER_HEIGHT*2);
+            if(unMovableLeftFatherRoom()){moveX(0);}
+            else{this.moveX(UNIT_SIZE);}
+        }
+        else this.animation.stop();
+    }
+    public boolean unMovableUpFatherRoom() {
+        if (!GameView.direction.equals(GameView.Direction.down)) {
+            //���
+            if (this.getTranslateY() == 0) { return true; }
+            //
+            else {return false;}
+        }
+        else {return false;}
+    }
+    public boolean unMovableDownFatherRoom() {
+        if (!GameView.direction.equals(GameView.Direction.up)) {
+            //���
+            if (this.getTranslateY()+PLAYER_HEIGHT==GameView.GAME_HEIGHT) { return true; }
+            //
+            else {return false;}
+        }
+        else {return false;}
+    }
+    public boolean unMovableLeftFatherRoom() {
+        if (!GameView.direction.equals(GameView.Direction.right)) {
+            //���
+            if (this.getTranslateX() == 0) { return true; }
+            //���U�d�l
+            //
+            else {return false;}
+        }
+        else {return false;}
+    }
+    public boolean unMovableRightFatherRoom() {
+        if (!GameView.direction.equals(GameView.Direction.left)) {
+            //���
+            if (this.getTranslateX()+PLAYER_WIDTH ==GameView.GAME_WIDTH) { return true; }
+            //
+            else {return false;}
+        }
+        else {return false;}
+    }
+    public void getLocation(int playerX, int playerY){
+
+    }
+
+}
